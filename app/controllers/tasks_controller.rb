deb2feb2ea0c12006b0e4316333da4e0a73e1f1e
attom <atten.atom@gmail.com>
2020-04-20 01:21:23 +0000

    login and post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc)
+    end
   end
 
   def show
@@ -15,6 +20,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
       redirect_to @task
@@ -41,16 +47,19 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'Task は正常に消去されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
-end
 
 private
-
-  def set_task
-    @task = Task.find(params[:id])
-  end
 
   def task_params
     params.require(:task).permit(:content, :status)
   end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'tasks/create'
+  get 'tasks/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
   resources :tasks
+  resources :users, only: [:new, :create]
+  
+  resources :tasks, only: [:create, :destroy]
 end
